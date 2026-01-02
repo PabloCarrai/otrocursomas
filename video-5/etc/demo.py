978929b214ffa3c86b5df14e3d67bc3038d789a8
PabloCarrai <pablohernanjuan@hotmail.com>
2026-01-02 11:35:31 -0300

    Va despacio pero va bien

---
+++ video-5/etc/demo.py
@@ -0,0 +1,4 @@
+import math  #   Importo el modulo math
+
+print(math.e)  #    Imprimo el valor de la constante e
+print(math.pi)  #   Imprimo el valor de la constante pi
---
+++ video-6/ejercicio-6.py
@@ -0,0 +1,9 @@
+import math  #   Importo el modulo math
+
+#   Muestra el valor de la constante euler
+print(math.e)
+#   Imprimo el valor de la constante pi
+print(math.pi)
+
+# Calculo el seno de pi
+print(math.sin(math.pi))
