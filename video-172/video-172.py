55924242359b5ca6e62710eff0a7d059912bdbcc
PabloCarrai <pablohernanjuan@hotmail.com>
2026-01-22 11:10:00 -0300

    Ahi quedaria. Aunque el numero esta mal.

---
+++ video-172/video-172.py
@@ -0,0 +1,5 @@
+#   esto es del 173, el tipo envio un amage malisimo
+#   input()
+nombre = input("Digite su nombre:  ")
+print(f"El tipo de dato de la variable nombre es: {type(nombre).__name__}")
+print(f"El contenido de la variable nombre es: {nombre}")
