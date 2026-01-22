18da4134a2462ead9d6da750129b799edef148e5
PabloCarrai <pablohernanjuan@hotmail.com>
2026-01-22 10:51:19 -0300

    Agregando video 170, si ya se en una branch nuevo, pero nada hay que practicar lo aprendido

---
+++ video-170/video-170.py
@@ -0,0 +1,13 @@
+#   Convertir expresiones algebraicas a expresiones programaticas
+a = 1
+b = 2
+c = 3
+d = 16
+e = 7
+resultado = a / (b * c) / d ** (1 / 2) ** 3
+resuldato1 = (a**3) ** 2 - b * c / (d * e)
+print("---------------------------------------")
+print(f"Resultado: {resultado}")
+print("---------------------------------------")
+print(f"Resultado: {resuldato1}")
+print("---------------------------------------")
