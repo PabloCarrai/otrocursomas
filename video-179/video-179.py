5be6e26e97315cc3c55bc811468cd7f48fc058e7
PabloCarrai <pablohernanjuan@hotmail.com>
2026-01-22 13:54:17 -0300

    Agregango los archivos

---
+++ video-179/video-179.py
@@ -0,0 +1,13 @@
+#   Condicionales
+edad = 17
+if edad < 18:  # si es true
+    print("Usted es Menor de edad.")  # corre esto
+else:  # sino
+    print("Usted es Mayor de edad")  # corre esto
+
+edad = 29  #   Cambio el valor y cambia la condicion
+if edad < 18:  # si es true
+    print("Usted es Menor de edad.")  # corre esto    
+else:  # sino
+    print("Usted es Mayor de edad")  # corre esto
+    print("Puede entrar a la disco")
---
+++ video-180/video-180.py
@@ -0,0 +1,6 @@
+# condicionales para ver si un numero es mayor a otro
+numero = int(input("Ingresa un numero   "))
+numero1 = int(input("Ingresa otro numero   "))
+if numero > numero1:
+    print(f"El primer numero {numero} es mayor al segundo {numero1}")
+print("El programa ha finalizado")
