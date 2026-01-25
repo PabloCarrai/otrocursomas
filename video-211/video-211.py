e20970fcb1366afe82cfed1245d8b386ea72dd00
PabloCarrai <pablohernanjuan@hotmail.com>
2026-01-24 23:39:28 -0300

    va primer ejercicio

---
+++ video-211/video-211.py
@@ -0,0 +1,9 @@
+#   Generar el patron de caracteres asterisco
+arranca = 1
+while arranca <= 5:
+    print("*" * arranca)
+    arranca += 1
+arranca = 5
+while arranca >= 1:
+    print("*" * arranca)
+    arranca -= 1
