f119a1310f71babd36d86804181c218ff0168c3b
PabloCarrai <pablohernanjuan@hotmail.com>
2026-01-24 23:15:05 -0300

    va

--- video-210/video-210.py
+++ video-210/video-210.py
@@ -1,13 +1,12 @@
 from random import randint as rd
 
-aleatorioAdiviar = rd(1, 10)
+aleatorioAdiviar = rd(1, 15)
 print(aleatorioAdiviar)
 intentos = 3
 while intentos >= 1:
-    numero = int(input("Adivine un numero entre el 1 y el 10  "))
+    numero = int(input("Adivine un numero entre el 1 y el 15  "))
     intentos -= 1
     if numero != aleatorioAdiviar:
-        print(f"Salio el {aleatorioAdiviar} y vos elegistes el {numero}")
         print(f"Tienes {intentos}")
     else:
         if numero == aleatorioAdiviar:
