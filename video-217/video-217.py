3ddd2225b0280131614ef2ea337f33cdce8ad401
PabloCarrai <pablohernanjuan@hotmail.com>
2026-01-26 14:41:08 -0300

    Vamos loco, este me salio

---
+++ video-217/video-217.py
@@ -0,0 +1,15 @@
+#   Dibujar una E en asteriscos
+patrones = ["*", "****", "*****"]
+llamar = [
+    patrones[2],
+    patrones[0],
+    patrones[0],
+    patrones[1],
+    patrones[0],
+    patrones[0],
+    patrones[2],
+]
+for e in llamar:
+    print(e)
+
+#   Este lo resolvi sin ver el video, bien ahi
