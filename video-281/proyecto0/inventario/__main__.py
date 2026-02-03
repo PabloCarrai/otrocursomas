03c0d1f1992969e26ccd044b217f7b59028c9e3e
PabloCarrai <pablohernanjuan@hotmail.com>
2026-02-03 18:17:16 -0300

    va

---
+++ video-281/proyecto0/inventario/__main__.py
@@ -0,0 +1,142 @@
+from inventario_funciones import (
+    registrar_producto,
+    realizar_venta,
+    buscar_productos,
+    cambiar_estado_producto,
+    ventas_rango_fecha,
+    top_5_mas_vendidos,
+    top_5_menos_vendidos,
+)
+
+
+def mostrar_menu():
+    """
+    Muestra menu de opciones disponibles
+    """
+    print("1. Registrar nuevo producto  ")
+    print("2. Vender un producto  ")
+    print("3. Buscar un producto por su codigo  ")
+    print("4. Cambiar disponibilidad de un producto  ")
+    print("5. Productos vendidos en un rango de fechas  ")
+    print("6. Top 5 de productos mas vendidos  ")
+    print("7. Top 5 de productos menos vendidos  ")
+    print("0. Salir  ")
+
+
+def capturar_entero(mensaje):
+    """
+    Captura un numero entero, valida el ingreso de datos
+    Parameters:
+    mensaje: Mensaje o texto personalizado a mostrar para la captura de un numero entero.
+    Returns:
+    Numero entero resultado de la captura
+    """
+    while True:
+        try:
+            numero = int(input(f"{mensaje: }"))
+            return numero
+        except ValueError:
+            print("Error debe digitar un numero entero.")
+        print()
+
+
+def capturar_real(mensaje):
+    """
+    Captura un numero real, valida el ingreso de datos
+    Parameters:
+    mensaje: Mensaje o texto personalizado a mostrar para la captura de un numero real.
+    Returns:
+    Numero entero resultado de la captura
+    """
+    while True:
+        try:
+            numero = float(input(f"{mensaje: }"))
+            return numero
+        except ValueError:
+            print("Error debe digitar un numero entero.")
+        print()
+
+
+def capturar_cadena(mensaje):
+    """
+    Captura un cadena de caracteres, valida el ingreso de datos
+    Parameters:
+    mensaje: Mensaje o texto personalizado a mostrar para la captura de un cadena de caracteres.
+    Returns:
+    Cadena de caracteres resultado de la captura
+    """
+    while True:
+        cadena = input(f"{mensaje: }").strip()
+        if len(cadena) != 0:
+            return cadena
+        else:
+            print("Debe digitar una cadena de caracteres con texto")
+        print()
+
+
+def main():
+    productos = []
+    ventas = []
+    while True:
+        while True:
+            try:
+                mostrar_menu()
+                opcion = int(input("Digite una opcion:    "))
+                if 0 <= opcion <= 7:
+                    break
+                else:
+                    print("Debe digitar un numero mayor o igual a 0")
+            except ValueError as e:
+                print("Error debe digitar un entero valido   ")
+        if opcion == 0:
+            break
+        elif opcion == 1:
+            while True:
+                id_producto = capturar_entero("Ingrese el id del nuevo producto:")
+                producto = buscar_productos(productos, id_producto)
+                if producto is None:
+                    break
+                else:
+                    print("Ya existe un producto con el id Digitado")
+            nombre_producto = capturar_cadena("Digite el nombre del nuevo producto")
+
+            while True:
+                precio = capturar_real("Digite el precio del nuevo producto")
+                if precio > 0:
+                    break
+                else:
+                    print("Debe digitar un precio de producto mayor a 0")
+            while True:
+                cantidad_producto = capturar_entero(
+                    "Digite la cantidad del nuevo producto"
+                )
+                if cantidad_producto >= 0:
+                    break
+                else:
+                    print("Debe digitar una cantidad positiva")
+            while True:
+                print("1.Disponible")
+                print("2.No Disponible")
+                disponible = capturar_entero(
+                    "Digite la opcion para la disponibilidad del producto"
+                )
+                if disponible == 1 or disponible == 2:
+                    disponible = disponible == 1
+                    break
+
+            nuevo_producto = {
+                "id_producto": id_producto,
+                "nombre": nombre_producto,
+                "precio": precio,
+                "cantidad": cantidad_producto,
+                "disponible": disponible,
+            }
+            registrar_producto(producto, nuevo_producto)
+            print("El producto se ha creado de forma satisfactoria")
+
+    print()
+    print("El programa ha finalizado")
+
+
+if __name__ == "__main__":
+    main()
---
+++ video-281/proyecto0/inventario/inventario_funciones.py
@@ -0,0 +1,119 @@
+from datetime import datetime
+from collections import Counter
+
+
+def registrar_producto(productos, producto):
+    """
+    Registra un nuevo producto en el inventario
+
+    Parameters:
+    productos: Lista de productos en el inventario
+    producto: Producto a agrgar al inventario
+    """
+    productos.append(producto)
+
+
+def realizar_venta(ventas, venta):
+    """
+    Crea una nueva venta
+
+    Parameters:
+    ventas: lista de las ventas realizadas hasta el momento
+    venta: venta recien realizada
+    """
+    venta["fecha"] = datetime.now()
+    ventas.append(venta)
+
+
+def buscar_productos(productos, id_producto):
+    """
+    Busca un producto a partir de su id
+    Parameters:
+    productos: lista de productos en el inventario
+    id_producto: id del producto a buscar
+    Returns:
+    el producto encontrado, si no se encuentra None
+    """
+    for p in productos:
+        if p["id_producto"] == id_producto:
+            return p
+
+    return None
+
+
+def cambiar_estado_producto(producto):
+    """
+    Cambia el estado de un producto
+    Parameters:
+    producto: producto sobre el que se cambiara el estado
+    """
+    producto["disponible"] = not producto["disponible"]
+
+
+def ventas_rango_fecha(ventas, fecha_inicio, fecha_final):
+    """
+    Obtiene las ventas realizadas en un rango de fecha
+
+    Parameters:
+    ventas: lista de ventas realizadas hasta el momento
+    fecha_inicio: fecha de inicio del rango
+    fecha_final: fecha final del rango
+
+    Returns:
+    lista de ventas realizadas en el rango especificado
+
+    """
+    ventas_rango = []
+    for v in ventas:
+        if fecha_inicio <= v["fecha"] <= fecha_final:
+            ventas_rango.append(v)
+    return ventas_rango
+
+
+def top_5_mas_vendidos(ventas):
+    """
+    Obtiene el top 5 de los mas vendidos
+
+    Parameters:
+    ventas: Lista de ventas realizadas hasta el momento
+    Returns:
+    Lista de tuplas (id,cantidad_total_venta) de los 5 productos mas vendidos
+    """
+    conteo_ventas = {}
+    for v in ventas:
+        if v["id_producto"] in conteo_ventas:
+            conteo_ventas[v["id_producto"]] += v["cantidad"]
+        else:
+            conteo_ventas[v["id_producto"]] = v["cantidad"]
+
+    conteo_ventas = {
+        k: v
+        for k, v in sorted(
+            conteo_ventas.items(), key=lambda item: item[1], reverse=True
+        )
+    }
+    contador = Counter(conteo_ventas)
+    return contador.most_common(5)
+
+
+def top_5_menos_vendidos(ventas):
+    """
+    Obtiene el top 5 de los menos vendidos
+
+    Parameters:
+    ventas: Lista de ventas realizadas hasta el momento
+    Returns:
+    Lista de tuplas (id,cantidad_total_venta) de los 5 productos menos vendidos
+    """
+    conteo_ventas = {}
+    for v in ventas:
+        if v["id_producto"] in conteo_ventas:
+            conteo_ventas[v["id_producto"]] += v["cantidad"]
+        else:
+            conteo_ventas[v["id_producto"]] = v["cantidad"]
+
+    conteo_ventas = {
+        k: v for k, v in sorted(conteo_ventas.items(), key=lambda item: item[1])
+    }
+    contador = Counter(conteo_ventas)
+    return contador.most_common()[:-6:-1]
