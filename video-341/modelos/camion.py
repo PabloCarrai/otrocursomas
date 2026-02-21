48b3482230a6d659a99b106ebbc3abe3777e57c5
PabloCarrai <pablohernanjuan@hotmail.com>
2026-02-21 12:11:29 -0300

    va

---
+++ video-341/modelos/camion.py
@@ -0,0 +1,14 @@
+from carro import Carro
+
+
+class Camion(Carro):
+    def __init__(
+        self,
+        placa,
+        marca,
+        modelo,
+        pais_procedencia,
+        capacidad_carga,
+    ):
+        super().__init__(placa, marca, modelo, pais_procedencia)
+        self.capacidad_carga = capacidad_carga
