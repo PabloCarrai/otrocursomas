d8ba49be3d61bc0463aa4b6874ff86cd692c8c20
PabloCarrai <pablohernanjuan@hotmail.com>
2026-02-22 14:54:49 -0300

    va

---
+++ video-343/modelos/volqueta.py
@@ -0,0 +1,16 @@
+from carro import Carro
+
+
+class Volqueta(Carro):
+    def __init__(
+        self, placa, marca, modelo, pais_procedencia, capacidad_carga, costo_servicio
+    ):
+        super().__init__(placa, marca, modelo, pais_procedencia)
+        self.capacidad_carga = capacidad_carga
+        self.costo_servicio = costo_servicio
+
+    def cargar_material(self):
+        print("Cargando Material....")
+
+    def descargar_material(self):
+        print("Descargando Material....")
