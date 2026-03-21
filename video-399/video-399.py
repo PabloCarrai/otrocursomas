799a85de887b2296ca1409370a7156f73615c79b
PabloCarrai <pablohernanjuan@hotmail.com>
2026-03-21 19:41:36 -0300

    va

---
+++ video-399/video-399.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt5.QtWidgets import (
+    QApplication,
+    QLabel,
+    QLineEdit,
+    QMessageBox,
+    QMainWindow,
+    QPushButton,
+)
+
+
+class CalculadoraVentana(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.initUi()
+
+    def initUi(self):
+        self.setWindowTitle("Calculadora Basica")
+        self.setFixedSize(400, 350)
+
+
+def main():
+    app = QApplication(sys.argv)
+    calculadora = CalculadoraVentana()
+    calculadora.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
