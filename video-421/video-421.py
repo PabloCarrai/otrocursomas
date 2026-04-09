df5958c7158796b2f14b639a06b5a6e1c6d0335a
PabloCarrai <pablohernanjuan@hotmail.com>
2026-04-09 20:44:40 -0300

    va

---
+++ video-421/video-421.py
@@ -0,0 +1,40 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QColorDialog
+from PyQt5.QtGui import QColor
+
+
+class SeleccionColorVentana(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.inicializarGui()
+
+    def inicializarGui(self):
+        self.setWindowTitle("Seleccion de color")
+        self.setFixedSize(400, 200)
+
+        self.btn_seleccionar_color = QPushButton("Seleccionar color....", self)
+        self.btn_seleccionar_color.setFixedWidth(200)
+        self.btn_seleccionar_color.move(100, 30)
+        self.btn_seleccionar_color.clicked.connect(self.seleccionar_color)
+
+        self.lbl_resultado_seleccion = QLabel("", self)
+        self.lbl_resultado_seleccion.move(190, 100)
+        self.lbl_resultado_seleccion.setFixedWidth(100)
+
+    def seleccionar_color(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.lbl_resultado_seleccion.setText(color.name())
+            self.lbl_resultado_seleccion.setStyleSheet(f"color:{color.name()}")
+
+
+def main():
+    app = QApplication(sys.argv)
+    ventana = SeleccionColorVentana()
+    ventana.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
