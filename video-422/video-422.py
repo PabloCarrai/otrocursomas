2e14101078c2163e600eeef729d6bb884872cfe6
PabloCarrai <pablohernanjuan@hotmail.com>
2026-04-11 21:41:14 -0300

    va

---
+++ video-422/video-422.py
@@ -0,0 +1,39 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFontDialog, QLabel, QPushButton
+
+
+class SeleccionFuenteVentana(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.inicializarGui()
+
+    def inicializarGui(self):
+        self.setWindowTitle("Seleccion de Fuente")
+        self.setFixedSize(400, 250)
+
+        self.btn_seleccionar_fuente = QPushButton("Seleccionar Fuente", self)
+        self.btn_seleccionar_fuente.move(100, 30)
+        self.btn_seleccionar_fuente.setFixedWidth(200)
+        self.btn_seleccionar_fuente.clicked.connect(self.seleccionar_fuente)
+
+        self.lbl_resultado_seleccion = QLabel("", self)
+        self.lbl_resultado_seleccion.move(30, 150)
+        self.lbl_resultado_seleccion.setFixedWidth(250)
+
+    def seleccionar_fuente(self):
+        fuente, ok = QFontDialog.getFont()
+        if ok:
+            self.lbl_resultado_seleccion.setText(fuente.toString())
+            self.lbl_resultado_seleccion.setFont(fuente)
+
+
+def main():
+    app = QApplication(sys.argv)
+    ventana = SeleccionFuenteVentana()
+    ventana.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
