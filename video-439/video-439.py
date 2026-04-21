01b4c3e8023b827980f5a5a310e536bc000d0b49
PabloCarrai <pablohernanjuan@hotmail.com>
2026-04-21 16:07:01 -0300

    va

--- video-439/video-439.py
+++ video-439/video-439.py
@@ -1,5 +1,15 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QTabWidget, QLineEdit, QWidget
+from PyQt5.QtWidgets import (
+    QApplication,
+    QTabWidget,
+    QLineEdit,
+    QWidget,
+    QFormLayout,
+    QRadioButton,
+    QHBoxLayout,
+    QComboBox,
+    QPushButton,
+)
 
 
 class CapturaDatos(QTabWidget):
@@ -24,13 +34,35 @@ class CapturaDatos(QTabWidget):
         self.inicializar_tab_datos_educacion()
 
     def inicializar_tab_datos_basicos(self):
-        pass
+        layout = QFormLayout()
+        layout.addRow("Nombre:", QLineEdit())
+        layout.addRow("Apellido:", QLineEdit())
+
+        lay_genero = QHBoxLayout()
+        lay_genero.addWidget(QRadioButton("Hombre"))
+        lay_genero.addWidget(QRadioButton("Mujer"))
+        layout.addRow("Genero", lay_genero)
+        self.tab_datos_basicos.setLayout(layout)
 
     def inicializar_tab_datos_contacto(self):
-        pass
+        layout = QFormLayout()
+        layout.addRow("Correo-E:", QLineEdit())
+        layout.addRow("Direccion:", QLineEdit())
+        self.tab_datos_contacto.setLayout(layout)
 
     def inicializar_tab_datos_educacion(self):
-        pass
+        layout = QFormLayout()
+        cbx_nivel_educativo = QComboBox()
+        cbx_nivel_educativo.addItem("Primaria")
+        cbx_nivel_educativo.addItem("Secundaria")
+        cbx_nivel_educativo.addItem("Profesional")
+        cbx_nivel_educativo.addItem("Maestria")
+        cbx_nivel_educativo.addItem("Doctorado")
+        layout.addRow("Nivel educativo: ", cbx_nivel_educativo)
+        layout.addRow("Profesion: ", QLineEdit())
+        layout.addRow("", QPushButton("Guardar Datos"))
+
+        self.tab_datos_educacion.setLayout(layout)
 
 
 def main():
