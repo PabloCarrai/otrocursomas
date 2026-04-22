2b9f5af18221aa87f9573aa98e7b2ba2952fade1
PabloCarrai <pablohernanjuan@hotmail.com>
2026-04-22 15:41:15 -0300

    va

---
+++ video-443/video-443.py
@@ -0,0 +1,50 @@
+import sys
+import random
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget
+from PyQt5.QtGui import QColor, QPainter, QPen
+from PyQt5.QtCore import Qt
+
+
+class LienzoDibujoVentana(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.inicializarGui()
+
+    def inicializarGui(self):
+        self.setWindowTitle("Lienzo Puntos Aleatorios")
+        self.setFixedSize(400, 400)
+
+        self.setAutoFillBackground(True)
+        paleta = self.palette()
+        paleta.setColor(self.backgroundRole(), Qt.white)
+        self.setPalette(paleta)
+
+        dibujo=DibujoWidget(self)
+        dibujo.move(0,0)
+        dibujo.resize(400,400)
+
+
+class DibujoWidget(QWidget):
+    def paintEvent(self, evento):
+        painter = QPainter(self)
+        painter.setPen(Qt.black)
+
+        tamagnio = self.size()
+
+        for _ in range(2048):
+            x = random.randint(1, tamagnio.width() - 1)
+            y = random.randint(1, tamagnio.height() - 1)
+
+            painter.drawPoint(x, y)
+
+
+def main():
+    app = QApplication(sys.argv)
+    ventana = LienzoDibujoVentana()
+    ventana.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
---
+++ video-445/video-445.py
@@ -0,0 +1,60 @@
+import sys
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QTableWidget,
+    QTableWidgetItem,
+    QVBoxLayout,
+    QWidget,
+)
+
+
+class TablaDatosVentana(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.inicializarGui()
+
+    def inicializarGui(self):
+        self.setWindowTitle("Tabla de Datos")
+        self.setFixedSize(400, 400)
+
+        self.creatTabla()
+
+    def creatTabla(self):
+        self.tbl_datos = QTableWidget()
+        self.tbl_datos.setRowCount(5)
+        self.tbl_datos.setColumnCount(3)
+
+        self.tbl_datos.setItem(0, 0, QTableWidgetItem("Dato 1"))
+        self.tbl_datos.setItem(0, 1, QTableWidgetItem("Dato 2"))
+        self.tbl_datos.setItem(0, 2, QTableWidgetItem("Dato 3"))
+        self.tbl_datos.setItem(1, 0, QTableWidgetItem("Dato 4"))
+        self.tbl_datos.setItem(1, 1, QTableWidgetItem("Dato 5"))
+        self.tbl_datos.setItem(1, 2, QTableWidgetItem("Dato 6"))
+        self.tbl_datos.setItem(2, 0, QTableWidgetItem("Dato 7"))
+        self.tbl_datos.setItem(2, 1, QTableWidgetItem("Dato 8"))
+        self.tbl_datos.setItem(2, 2, QTableWidgetItem("Dato 9"))
+
+        self.tbl_datos.doubleClicked.connect(self.seleccionar_elemento)
+
+        widget = QWidget(self)
+        self.setCentralWidget(widget)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.tbl_datos)
+        widget.setLayout(self.layout)
+
+    def seleccionar_elemento(self):
+        print("Se ha hecho doble clic sobre la tabla")
+
+
+def main():
+    app = QApplication(sys.argv)
+    ventana = TablaDatosVentana()
+    ventana.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
