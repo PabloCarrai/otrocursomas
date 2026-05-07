2cf5b35b13e81345cdf351739e70ffb7486a4fb2
PabloCarrai <pablohernanjuan@hotmail.com>
2026-05-07 13:15:22 -0300

    va

---
+++ video-556/video-556.py
@@ -0,0 +1,48 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QWidget
+
+
+class ArrastrarSoltarDialogo(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.inicializarGui()
+
+    def inicializarGui(self):
+        self.setWindowTitle("Arrastrar & Soltar")
+        self.setFixedSize(450, 150)
+
+        self.txt_entrada = QLineEdit("Arrastre este texto a la etiqueta", self)
+        self.txt_entrada.setDragEnabled(True)
+        self.txt_entrada.move(20, 30)
+        self.txt_entrada.resize(200, 32)
+
+        self.lbl_contenido = ContenedorLabel("Arrastre aqui", self)
+        self.lbl_contenido.move(230, 35)
+        self.lbl_contenido.resize(200,32)
+
+
+class ContenedorLabel(QLabel):
+    def __init__(self, texto, padre):
+        super().__init__(texto, padre)
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, evento):
+        if evento.mimeData().hasFormat("text/plain"):
+            evento.accept()
+        else:
+            evento.ignore()
+
+    def dropEvent(self, evento):
+        self.setText(evento.mimeData().text())
+
+
+def main():
+    app = QApplication(sys.argv)
+    ventana = ArrastrarSoltarDialogo()
+    ventana.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
