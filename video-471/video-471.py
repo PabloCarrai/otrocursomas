42bcdfa6201d1fe6f449b141f6f3fd38749dcbe5
PabloCarrai <pablohernanjuan@hotmail.com>
2026-05-23 14:50:10 -0300

    ja

---
+++ video-471/video-471.py
@@ -0,0 +1,40 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from video471 import Ui_Pizzeria
+
+
+class AplicacionPizzeria(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.inicializarGui()
+
+    def inicializarGui(self):
+        self.ui = Ui_Pizzeria()
+        self.ui.setupUi(self)
+
+        self.ui.chk_queso.stateChanged.connect(self.calcular_precio)
+        self.ui.chk_salsas.stateChanged.connect(self.calcular_precio)
+        self.ui.chk_aceitunas.stateChanged.connect(self.calcular_precio)
+
+    def calcular_precio(self):
+        precio = 15
+        if self.ui.chk_queso.isChecked():
+            precio += 1
+        if self.ui.chk_aceitunas.isChecked():
+            precio += 2
+        if self.ui.chk_salsas.isChecked():
+            precio += 1
+
+        self.ui.lbl_precio_final.setText(f"Precio Final: {precio}")
+
+
+def main():
+    app = QApplication(sys.argv)
+    ventana = AplicacionPizzeria()
+    ventana.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
