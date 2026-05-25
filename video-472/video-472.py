72b192c8ec3eacdd1518447f3972e5cc3f754f3e
PabloCarrai <pablohernanjuan@hotmail.com>
2026-05-24 21:41:15 -0300

    va

---
+++ video-472/video-472.py
@@ -0,0 +1,48 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
+from video472 import Ui_InicioSesion
+
+
+class AplicacionInicioSesion(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.inicializarGui()
+
+    def inicializarGui(self):
+        self.ui = Ui_InicioSesion()
+        self.ui.setupUi(self)
+
+        self.ui.btn_iniciar_sesion.clicked.connect(self.iniciar_sesion)
+
+    def iniciar_sesion(self):
+        nombre_usuario = self.ui.txt_nombre_usuario.text().strip()
+        contrasegnia = self.ui.txt_contrasegnia.text().strip()
+        mensaje = QMessageBox(self)
+        mensaje.setWindowTitle("Mensaje")
+
+        if len(nombre_usuario) and len(contrasegnia):
+            if nombre_usuario == "pablo" and contrasegnia == "pabl0":
+                mensaje.setIcon(QMessageBox.Information)
+                mensaje.setText("Bienvenido")
+            else:
+                mensaje.setIcon(QMessageBox.Warning)
+                mensaje.setText("Credenciales escritas invalidas")
+
+                pass
+        else:
+            mensaje.setIcon(QMessageBox.Warning)
+            mensaje.setText("Todos los campos son obligatorio")
+
+        mensaje.exec_()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ventana = AplicacionInicioSesion()
+    ventana.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
