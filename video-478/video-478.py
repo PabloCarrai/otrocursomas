19b478a45f1c2bc01cb7c3bf0a6f7cde71b337fb
PabloCarrai <pablohernanjuan@hotmail.com>
2026-07-21 20:04:03 -0300

    va

---
+++ video-478/video-478.py
@@ -0,0 +1,42 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog
+from video478 import Ui_Niveles
+
+
+class AplicacionNiveles(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.inicializarGui()
+
+    def inicializarGui(self):
+        self.ui = Ui_Niveles()
+        self.ui.setupUi(self)
+
+        self.ui.hsb_nivel_azucar.valueChanged.connect(self.mostrar_nivel_azucar)
+        self.ui.hsd_presion_arterial.valueChanged.connect(self.mostrar_presion_arterial)
+        self.ui.vsb_pulso.valueChanged.connect(self.mostrar_pulso)
+        self.ui.vsd_nivel_colesterol.valueChanged.connect(self.mostrar_nivel_colesterol)
+
+    def mostrar_nivel_azucar(self, valor):
+        self.ui.txt_resultado.setText(f"Nivel de azucar: {valor}")
+
+    def mostrar_presion_arterial(self, valor):
+        self.ui.txt_resultado.setText(f"Presion arterial: {valor}")
+
+    def mostrar_pulso(self, valor):
+        self.ui.txt_resultado.setText(f"Pulso: {valor}")
+
+    def mostrar_nivel_colesterol(self, valor):
+        self.ui.txt_resultado.setText(f"Pulso: {valor}")
+
+
+def main():
+    app = QApplication(sys.argv)
+    ventana = AplicacionNiveles()
+    ventana.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
