500f2ce2d056cf7ca71052ebf63a947664a0f9d4
PabloCarrai <pablohernanjuan@hotmail.com>
2026-08-17 11:42:29 -0300

    ahi va

---
+++ video-484/video-484.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtGui import QFont
+from video484 import Ui_EditorTexto
+
+
+class AplicacionEditorTexto(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.inicializarGui()
+
+    def inicializarGui(self):
+        self.ui = Ui_EditorTexto()
+        self.ui.setupUi(self)
+
+        self.ui.cbx_fuentes.currentIndexChanged.connect(self.cambiar_fuente)
+
+    def cambiar_fuente(self):
+        indice = self.ui.cbx_fuentes.currentIndex()
+        fuente = QFont(self.ui.cbx_fuentes.itemText(indice), 15)
+        self.ui.txt_editor.setFont(fuente)
+
+
+def main():
+    app = QApplication(sys.argv)
+    ventana = AplicacionEditorTexto()
+    ventana.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
